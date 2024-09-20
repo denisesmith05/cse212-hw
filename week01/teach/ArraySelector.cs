c6937ac041dd1763a55fb8bd9ba30860c8fca7d7
Denise <denise.sunshine@icloud.com>
2024-09-20 10:51:23 -0400

    add array selector

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -8,9 +8,21 @@ public static class ArraySelector
         var intResult = ListSelector(l1, l2, select);
         Console.WriteLine("<int[]>{" + string.Join(", ", intResult) + "}"); // <int[]>{1, 2, 3, 2, 4, 4, 6, 8, 10, 5}
     }
-
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        var newList = new List<int>();
+        var index1 = 0;
+        var index2 = 0;
+        
+        foreach ( var number in select){
+            if (number == 1) {
+                newList.Add(list1[index1]);
+                index1++;
+            } else {
+                newList.Add(list2[index2]);
+                index2++;
+            }
+        }
+        return newList.ToArray();
     }
 }
